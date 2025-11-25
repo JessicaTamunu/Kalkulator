4f28da33141df171074377a4f7882794c3e5b9e7
ShirenKeysiKorah <shirenkorah026@student.unsrat.ac.id>
2025-11-25 13:50:50 +0800

    Main.py

---
+++ Main.py
@@ -0,0 +1,13 @@
+def kali(a, b):
+    return a * b
+
+def bagi(a, b):
+    if b == 0:
+        return "Error: Tidak bisa membagi dengan nol!"
+    return a / b
+
+if pilihan == "3":
+    print("Hasil:", kali(angka1, angka2))
+
+elif pilihan == "4":
+  print("Hasil:", bagi(angka1, angka2))
