010065c646831853ccfb86807f5567f5aa09709a
JessicaTamunu <jessicatamunu026@student.unsrat.ac.id>
2025-11-25 13:48:31 +0800

    Main.py

--- Main.py
+++ Main.py
@@ -11,3 +11,6 @@ pilihan = input("Pilih operasi (1/2/3/4): ")
 
 angka1 = float(input("Masukkan angka pertama: "))
 angka2 = float(input("Masukkan angka kedua: "))
+
+if pilihan == "1":
+    print("Hasil:", tambah(angka1, angka2))
